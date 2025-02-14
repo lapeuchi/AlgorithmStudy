1d2d391da67f944100ae8ed804a1874f4ced99c7
lapeuchi <78643629+lapeuchi@users.noreply.github.com>
2025-02-14 17:37:00 +0900

    Create 2609.cpp

---
+++ Baekjoon/2609/2609.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) 
+{
+	int n;
+
+	while (b != 0) 
+	{
+		n = a % b;
+		a = b;
+		b = n;
+	}
+
+	return a;
+}
+
+int lcm(int a, int b) 
+{
+	return a * b / gcd(a, b);
+}
+
+int main() 
+{
+	int a, b;
+	cin >> a >> b;
+
+	cout << gcd(a, b) << "\n" << lcm(a, b);
+
+}

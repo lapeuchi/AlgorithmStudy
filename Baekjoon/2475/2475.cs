582661ff63b38e7811338f62e7045965206cf8b1
lapeuchi <78643629+lapeuchi@users.noreply.github.com>
2025-02-14 15:29:00 +0900

    Create 2475.cs

---
+++ Baekjoon/2475/2475.cs
@@ -0,0 +1,31 @@
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split();
+        int[] password = new int[6];
+
+        for (int i = 0; i < 5; i++)
+        {
+            password[i] = int.Parse(input[i]);
+        }
+
+        password[5] = Cal(password);
+        Console.Write(password[5]);
+    }
+
+    public static int Cal(int[] pw)
+    {
+        int check =
+            (int)(Math.Pow(pw[0], 2) +
+            Math.Pow(pw[1], 2) +
+            Math.Pow(pw[2], 2) +
+            Math.Pow(pw[3], 2) +
+            Math.Pow(pw[4], 2)) % 10;
+
+        return check;
+    }
+
+}

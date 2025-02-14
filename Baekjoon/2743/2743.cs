ca8b59b46c8dc93d6b6278f966aad0f0128eae82
lapeuchi <78643629+lapeuchi@users.noreply.github.com>
2025-02-14 15:33:21 +0900

    Create 2743.cs

---
+++ Baekjoon/2743/2743.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        string str = Console.ReadLine();
+
+        Console.WriteLine(str.Length);
+    }   
+}
+

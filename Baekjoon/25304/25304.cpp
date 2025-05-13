40161ffa3f4bb44d79f0e6b18626c9df93895d38
lapeuchi <78643629+lapeuchi@users.noreply.github.com>
2025-05-13 15:51:19 +0900

    Create 25304.cpp

---
+++ Baekjoon/25304/25304.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	int n;
+	
+	cin >> x;
+	cin >> n;
+
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		
+		sum += a * b;
+	}
+	
+	if (x == sum)
+	{
+		cout << "Yes";
+	}
+	else
+		cout << "No";
+	
+}
